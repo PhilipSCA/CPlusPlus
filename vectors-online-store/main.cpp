838e726719414d3a42fc700b1c6ac68c82f56364
y43d <philanderson556@gmail.com>
2021-07-05 11:41:36 +0100

    checkout / store program!

--- vectors-online-store/main.cpp
+++ vectors-online-store/main.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 #include <vector>
 #include "Colours.h"
+#include <string>
+#include <algorithm>
 
+std::vector<std::vector<std::string>> products = {
+	{"Doughnut","1.99"},
+	{"Sugar","1.50"},
+	{"Milk","1.29"},
+	{"Butter","1.00"},
+	{"Chocolate","4.00"},
+	{"Ketchup","2.40"}
+};
 
-
+//std::string products[6][2]{ {"Doughnut","1.99"}, {"Sugar","1.50"}, {"Milk","1.29"}, {"Butter","1.00"}, {"Chocolate","4.00"}, {"Ketchup","2.40"} };
 std::vector<float> orderPrices;
 std::vector<std::string> orderItems;
 
-void addToOrder(std::string item, float price) {
+void addToOrder(std::string item) {
 
-	orderPrices.push_back(price);
+	float priceToAdd;
+	for (int i = 0; i < products.size(); i++) {
+		if (products[i][0] == item) {
+			priceToAdd = std::stof(products[i][1]);
+			break;
+		}
+	}
+
+	orderPrices.push_back(priceToAdd);
 	orderItems.push_back(item);
 
 	float total = 0;
@@ -18,11 +36,7 @@ void addToOrder(std::string item, float price) {
 	}
 
 	std::cout << clrgreen << item << white << " added to order!\n";
-	std::cout << "Your basket contains:\n";
-	for (int i = 0; i < orderItems.size(); i++) {
-		std::cout << i << ". " << orderItems[i] << "\n";
-	}
-	std::cout << clrred << "\n========================\nOrder Total: " << clrgreen << "$" << total << clrred << "\n========================\n";
+	std::cout << clrred << "\n========================\nOrder Total: " << clrgreen << "$" << total << clrred << "\n========================\n" << white;
 }
 
 
@@ -31,54 +45,59 @@ int main() {
 	bool keepConverting = true;
 	char keepConvertingChar;
 	int choice;
+	float total = 0;
 
-
-	std::cout << clrred << "Welcome to Philip's store!\n" << white << "What would you like to purchase?\n";
+	std::cout << blue << "Welcome to Philip's store!\n" << white << "What would you like to purchase?\n";
 	while (keepConverting) {
-		
-		std::cout << clrred << "Menu:" << white << "\n";
-		std::cout << "1. Doughnut" << clrgreen << " - $1.99\n" << white;
-		std::cout << "2. Sugar" << clrgreen << " - $1.50\n" << white;
-		std::cout << "3. Milk" << clrgreen << " - $1.29\n" << white;
-		std::cout << "4. Butter" << clrgreen << " - $1.00\n" << white;
-		std::cout << "5. Chocolate" << clrgreen << " - £4.00\n" << white;
-		std::cout << "6. Ketchup" << clrgreen << " - £2.40\n\n" << white;
+
+		std::cout << clrred << "========================\nMENU\n" << white << "\n";
+		for (int x = 0; x < products.size(); x++) {
+			std::cout << x + 1 << ". " << products[x][0] << clrgreen << " - $" << products[x][1] << "\n" << white;
+		}
+		std::cout << clrred << "========================" << white << "\n\n";
 
 		std::cout << "Enter the ID number of what you would like to buy.\n";
 		std::cin >> choice;
-		while (std::cin.fail()) {
-			std::cout << clrred << "Input Error! Must be an integer.\n" << white;
+		while (std::cin.fail() || choice > (int)products.size() || choice < 0) {
+			std::cout << clrred << "Input Error! Must be an integer and on the menu.\n" << white;
 			std::cin.clear();
 			std::cin.ignore(256, '\n');
 			std::cin >> choice;
 		}
 
-		switch (choice) {
-		case 1:
-			addToOrder("Doughnut", 1.99);
-			break;
-		case 2:
-			addToOrder("Sugar", 1.50);
-			break;
-		case 3:
-			addToOrder("Milk", 1.29);
-			break;
+		addToOrder(products[choice - 1][0]);
 
-
-		default:
-			std::cout << "That isn't an option!";
-
-		}
-
-		std::cout << "\n\nWould you like to buy more? (y/n)\n";
+		std::cout << white << "\n\nWould you like to buy anything else? (y/n)\n";
 		std::cin >> keepConvertingChar;
 		if (keepConvertingChar == 'n') keepConverting = false;
+		system("cls");
 	}
 
-	std::cout << "Done!\n";
 
+	std::cout << "Your basket contains:\n";
+	for (int i = 0; i < orderItems.size(); i++) {
+		std::cout << i+1 << ". " << orderItems[i] << "\n";
+	}
+	for (int i = 0; i < orderPrices.size(); i++) {
+		total = total + orderPrices[i];
+	}
+	std::cout << clrred << "\n========================\nOrder Total: " << clrgreen << "$" << total << clrred << "\n========================\n";
+	
+	if (total == 0) {
+		std::cout << clrred << "Order is empty!\n";
+		Sleep(2000);
+		return 0;
+	}
+	
+	
+	for (int j = 0; j < 100; j++) {
+		std::cout << yellow << "\rAwaiting Payment   \rAwaiting Payment";
+		for (int i = 0; i < 3; i++) {
+			std::cout << ".";
+			Sleep(300);
+		}
+	}
+	
 
 	return 0;
 }
-
-
